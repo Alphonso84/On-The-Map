8e5011ba9812cd4011477d1fe8bdd3c0e765ceaf
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-06 14:52:56 -0800

    Code: setup Model Object for student location

---
+++ On The Map/Model/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  On The Map
+//
+//  Created by user on 1/6/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct OnTheMap {
+    let results: [Result]
+}
+
+struct StudentLocation {
+    let createdAt, firstName, lastName: String
+    let latitude, longitude: Double
+    let mapString, mediaURL, objectId, uniqueKey: String
+    let updatedAt: String
+}
--- On The Map/Networking/Networking.swift
+++ On The Map/Networking/Networking.swift
@@ -13,9 +13,9 @@ class Networking {
     
     var baseURL = "https://parse.udacity.com/parse/classes/StudentLocation" as! URL
     
-    func URLRequest() {
+    func Network() {
         
-        var request = URLRequest(url: baseURL)
+      var request = URL(fileURLWithPath: baseURL)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = URLSession.shared
