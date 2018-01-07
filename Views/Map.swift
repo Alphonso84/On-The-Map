c26398b282176148e5e5bed90a4e7ec5026a8a9a
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-06 18:57:21 -0800

    Code: Setup

---
+++ Views/Map.swift
@@ -0,0 +1,20 @@
+//
+//  Map.swift
+//  On The Map
+//
+//  Created by user on 1/6/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapClass: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
