9c25a91f8f738a73b875e4719e65be5b4c8b733c
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-02-18 15:28:10 -0800

    Update: parsing JSON data

--- On The Map/Model/Model.swift
+++ On The Map/Model/Model.swift
@@ -9,4 +9,30 @@
 import Foundation
 import UIKit
 
-
+struct ParseStudent: Codable {
+    
+    var firstName: String
+    var lastName: String
+    var longitude: Double
+    var latitude: Double
+    var mediaUrl: String
+    var mapString: String
+    var objectId: String
+    var uniqueKey: String
+    
+    var fullName: String {
+        return "\(firstName) \(lastName)"
+    }
+    
+    //Student from dictionary
+    init(dictionary: [String : AnyObject]) {
+        firstName = dictionary["firstName"] as! String!
+        lastName = dictionary["lastName"] as! String!
+        longitude = dictionary["longitude"] as! Double
+        latitude = dictionary["latitude"] as! Double
+        mediaUrl = dictionary["mediaURL"] as! String!
+        mapString = dictionary["mapString"] as! String!
+        objectId = dictionary["objectId"] as! String!
+        uniqueKey = dictionary["uniqueKey"] as! String!
+    }
+}
--- On The Map/Networking/Networking.swift
+++ On The Map/Networking/Networking.swift
@@ -21,20 +21,26 @@ class Networking {
     
     
     func getData() {
-        
-        
         var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = URLSession.shared
-        let task = session.dataTask(with: request) { data, response, error in
-            if error != nil { // Handle error...
-                return
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let unwrappedData = data else {return}
+            do {
+                 let jsonDecoder = JSONDecoder()
+                let jsonData = try JSONSerialization.jsonObject(with: unwrappedData, options: [])
+                print(jsonData)
+               
+            } catch {
+                print(error)
             }
-            print(String(data: data!, encoding: .utf8)!)
         }
-        task.resume()
+            task.resume()
+        
+        }
+    
 }
     
    
-}
+
